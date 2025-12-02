085fbad13793bb88afe6b1b15f61db013547c5da
Daniel Mendoza <daniel@danielmlozano.com>
2025-12-01 19:16:19 -0600

    dummy components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import TodoList from "./components/TodoList"
-
+import Title from "./components/Title"
 function App() {
 	return (
 		<div className="w-full min-h-screen bg-gray-900">
+			<Title title="Mis Tareas Pendientes" />
 			<TodoList />
 		</div>
 	)
---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+const Title = ({ title }) => {
+	return (
+		<h1 className="text-2xl font-bold text-white text-center pt-15">
+			{title}
+		</h1>
+	)
+}
+
+export default Title
